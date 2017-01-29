c74e18d1ee61af0b178c091eb6bcd541bf6f9f27
Thane Vo <vothane@gmail.com>
2017-01-29 00:34:56 +0000

    fix vector subtraction

--- kmeans.exs
+++ kmeans.exs
@@ -1,10 +1,10 @@
 defmodule KMeans do
 
   def distance(loc1, loc2) do
-    zipmult = fn(v1, v2) -> for {a, b} <- Enum.zip(v1, v2), do: a * b end
-    dot = fn(v1, v2) -> Enum.reduce(zipmult.(v1, v2), 0, &+/2) end
+    vect_mult = fn(v1, v2) -> for {a, b} <- Enum.zip(v1, v2), do: a * b end
+    dot = fn(v1, v2) -> Enum.reduce(vect_mult.(v1, v2), 0, &+/2) end
     sum_of_sqs = fn(v) -> dot.(v, v) end 
-    vect_diff = fn(v1, v2) -> for {a, b} <- Enum.zip(v1, v2), do: a * b end 
+    vect_diff = fn(v1, v2) -> for {a, b} <- Enum.zip(v1, v2), do: a - b end 
     sq_dist = fn(loc1, loc2) -> sum_of_sqs.(vect_diff.(loc1, loc2)) end
     
     :math.sqrt(sq_dist.(loc1, loc2))
@@ -32,12 +32,12 @@ defmodule KMeans do
   
   defp assign(k, inputs, means, prev_assignments, stop) when stop == false do 
     curr_assignments = for input <- inputs, do: classify(input, means)
-
-    for i <- 1..k do
-      i_pts = for {p, a} <- Enum.zip(inputs, curr_assignments), do: if a == i, do: p
-      unless Enum.any?(i_pts, &is_nil/1), do: means = List.replace_at(means, i, vector_mean(i_pts))  
-    end
-    
+    grouping = Enum.zip(inputs, curr_assignments)
+            |> Enum.group_by(fn {_, assign_id} -> assign_id end)
+    IO.inspect grouping 
+    means = Enum.reduce(grouping, means, fn({i, g}, m) -> 
+              List.replace_at(means, i, vector_mean(g)) end )  
+        
     assign(k, inputs, means, curr_assignments, prev_assignments == curr_assignments)
   end   
   
@@ -50,11 +50,13 @@ defmodule KMeansTest do
   use ExUnit.Case
 
   test "KMeans classify" do
-  
+    IO.puts "-----------------"
+    IO.inspect KMeans.distance([2, 2], [3, 3])
+    IO.puts "-----------------"
     inputs = [[-14,-5],[13,13],[20,23],[-19,-11],[-9,-16],[21,27],[-49,15],
               [26,13],[-46,5],[-34,-1],[11,15],[-49,0],[-22,-16],[19,28],
               [-12,-8],[-13,-19],[-41,8],[-11,-6],[-25,-9],[-18,-3]]
-    
+
     k3_means = [[-43.800000000000004, 5.4], 
                 [-15.888888888888888, -10.333333333333332], 
                 [18.333333333333332, 19.833333333333332]]
