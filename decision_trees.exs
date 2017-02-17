517962013746ecacf4aaef3bc8056bdfd6a32c38
Thane Vo <vothane@gmail.com>
2017-02-17 05:44:36 +0000

    implement class probablities from labels calculation

--- decision_trees.exs
+++ decision_trees.exs
@@ -4,7 +4,12 @@ defmodule DecisionTrees do
     Enum.map(class_probs, fn(p) -> -p * :math.log2(p) end)
     |> Enum.sum
   end 
-  
+ 
+  def class_probs(labels) do
+    total = Enum.count(labels)
+    Enum.reduce(labels, Map.new, fn(l, m) -> Map.update(m, l, 1, &(&1 + 1)) end) 
+    |> Enum.map(fn {_, count} -> count / total end) 
+  end  
 end  
 
 ExUnit.start
@@ -15,5 +20,9 @@ defmodule DecisionTreesTest do
   test "calculate entropy from class probabilities" do
     assert DecisionTrees.entropy([0.6, 0.4]) == 0.9709505944546686
   end
+
+  test "calculate class probabilities from labels" do
+    assert DecisionTrees.class_probs([false, false, false, true, true]) == [0.6, 0.4]
+  end  
 end
 
